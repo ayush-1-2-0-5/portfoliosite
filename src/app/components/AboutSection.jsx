5c53b2d4b27c34e684375c4e4d3dd3a7c19e4c56
Ayush <21ucs044@lnmiit.ac.in>
2024-11-28 23:32:43 +0530

    commit2

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useTransition, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
@@ -8,11 +8,11 @@ const TAB_DATA = [
     title: "Skills",
     id: "skills",
     content: (
-      <ul className="list-disc pl-2">
+      <ul className="list-disc pl-5">
         <li>Node.js</li>
         <li>Express</li>
         <li>PostgreSQL</li>
-        <li>Data structure</li>
+        <li>Data Structures</li>
         <li>Algorithms</li>
         <li>C++</li>
         <li>Python</li>
@@ -25,19 +25,8 @@ const TAB_DATA = [
     title: "Education",
     id: "education",
     content: (
-      <ul className="list-disc pl-2">
-        <li>B.Tech Computer Science and Engineering </li>
-        <li>The LNM Institute of Information Technology, India</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Certifications",
-    id: "certifications",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+      <ul className="list-disc pl-5">
+        <li>B.Tech Computer Science and Engineering<br/>The LNM Institute of Information Technology, India</li>
       </ul>
     ),
   },
@@ -45,44 +34,43 @@ const TAB_DATA = [
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
-  const [ startTransition] = useTransition();
 
   const handleTabChange = (id) => {
-    startTransition(() => {
-      setTab(id);
-    });
+    setTab(id); // Directly updating the state
   };
 
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} alt="About Image" />
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="About Image"
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
+            I am a full-stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
             working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            HTML, CSS, and Git. I am a quick learner and I am always looking to
+            HTML, CSS, and Git. I am a quick learner and always looking to
             expand my knowledge and skill set. I am a team player and excited to
             work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              Skills
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              Education
-            </TabButton>
+            {TAB_DATA.map((tabItem) => (
+              <TabButton
+                key={tabItem.id}
+                active={tab === tabItem.id}
+                selectTab={() => handleTabChange(tabItem.id)}
+              >
+                {tabItem.title}
+              </TabButton>
+            ))}
           </div>
           <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
+            {TAB_DATA.find((tabItem) => tabItem.id === tab)?.content}
           </div>
         </div>
       </div>
